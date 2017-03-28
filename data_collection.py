30ce0060ca11ea119c424b829532c9ac9e5d9fec
Mike Reilly <mpreilly@gatech.edu>
2017-03-28 11:28:52 -0400

    Created data_collection.py to consolidate my many files into one with different methods. Trying to clean it all up a bit.

---
+++ data_collection.py
@@ -0,0 +1,25 @@
+from yahoo_finance import Share
+import csv
+from datetime import date
+from datetime import timedelta
+
+def get_random_stocks(num_train_stocks, num_test_stocks):
+    all_companies = []
+    training_stocks = []
+    testing_stocks = []
+
+    with open('data/companylist.csv','rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            all_companies.append(row.pop(0))
+
+    for i in range(0, num_train_stocks):
+        training_stocks.append(all_companies.pop(random.randint(1, len(all_companies)-1)))
+
+    for i in range(0,num_test_stocks):
+        testing_stocks.append(all_companies.pop(random.randint(1, len(all_companies)-1)))
+
+    return training_stocks, testing_stocks
+
+
+def gather_historical(stocks, start_date, end_date):
